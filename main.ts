9e860a234f9c500ac57847eaf1f60cb4ef9f346e
lzty634158 <398310535@qq.com>
2018-07-05 09:25:28 +0800

    fix ultrasonic

--- main.ts
+++ main.ts
@@ -245,7 +245,7 @@ namespace mbit_传感器类 {
         pins.digitalWritePin(Trig, 0);
         control.waitMicros(2);
         pins.digitalWritePin(Trig, 1);
-        control.waitMicros(10);
+        control.waitMicros(15);
         pins.digitalWritePin(Trig, 0);
 
         // read pulse
@@ -907,7 +907,7 @@ namespace mbit_小车类 {
         pins.digitalWritePin(DigitalPin.P14, 0);
         control.waitMicros(2);
         pins.digitalWritePin(DigitalPin.P14, 1);
-        control.waitMicros(10);
+        control.waitMicros(15);
         pins.digitalWritePin(DigitalPin.P14, 0);
 
         // read pulse
