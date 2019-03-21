11422b2257dfd63b36ac8403a5c32897d07b5745
lzty634158 <lzty634158@github.com>
2019-03-21 18:12:56 +0800

    update csb

--- main.ts
+++ main.ts
@@ -241,16 +241,22 @@ namespace mbit_传感器类 {
     export function Ultrasonic(Trig: DigitalPin, Echo: DigitalPin): number {
 
         // send pulse
-        pins.setPull(Trig, PinPullMode.PullNone);
-        pins.digitalWritePin(Trig, 0);
-        control.waitMicros(2);
-        pins.digitalWritePin(Trig, 1);
-        control.waitMicros(15);
-        pins.digitalWritePin(Trig, 0);
 
-        // read pulse
-        let d = pins.pulseIn(Echo, PulseValue.High, 23200);
-        return  Math.floor(d / 58);
+        let list:Array<number> = [0, 0, 0, 0, 0];
+        for (let i = 0; i < 5; i++) {
+            pins.setPull(Trig, PinPullMode.PullNone);
+            pins.digitalWritePin(Trig, 0);
+            control.waitMicros(2);
+            pins.digitalWritePin(Trig, 1);
+            control.waitMicros(15);
+            pins.digitalWritePin(Trig, 0);
+    
+            let d = pins.pulseIn(Echo, PulseValue.High, 43200);
+            list[i] = Math.floor(d / 40)
+        }
+        list.sort();
+        let length = (list[1] + list[2] + list[3])/3;
+        return  Math.floor(length);
     }
 }
 
